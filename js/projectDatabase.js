caa5239aa29d0428cc336f16650e3ecaea8a52de
Dom Le <lequydung101@yahoo.com>
2020-10-28 13:18:24 +1100

    Add project database

---
+++ js/projectDatabase.js
@@ -0,0 +1,83 @@
+const projectDatabase = [
+  {
+    ID: 1,
+    projectName: "Form Validation",
+    linkURL: "formValidation",
+  },
+  {
+    ID: 2,
+    projectName: "Seat Booking",
+    linkURL: "seatBooking",
+  },
+  {
+    ID: 3,
+    projectName: "Exchange Rate Calculator",
+    linkURL: "ExchangeRateCalculator",
+  },
+  {
+    ID: 4,
+    projectName: "Hang Man Game",
+    linkURL: "hangManGame",
+  },
+  {
+    ID: 5,
+    projectName: "Meal Finder",
+    linkURL: "mealFinder",
+  },
+  {
+    ID: 6,
+    projectName: "Expense Tracker",
+    linkURL: "expenseTracker",
+  },
+  {
+    ID: 7,
+    projectName: "Music Player",
+    linkURL: "musicPlayer",
+  },
+  {
+    ID: 8,
+    projectName: "Speed Typing",
+    linkURL: "speedTyping",
+  },
+  {
+    ID: 9,
+    projectName: "Speech To Text",
+    linkURL: "SpeechToText",
+  },
+  {
+    ID: 10,
+    projectName: "Memory Card",
+    linkURL: "memoryCard",
+  },
+  {
+    ID: 11,
+    projectName: "Lyric Searching",
+    linkURL: "lyricSearching",
+  },
+  {
+    ID: 12,
+    projectName: "Relaxer App",
+    linkURL: "RelaxerApp",
+  },
+  {
+    ID: 13,
+    projectName: "Breaking Ball",
+    linkURL: "breakingBall",
+  },
+  {
+    ID: 14,
+    projectName: "Counting Down",
+    linkURL: "countingDown",
+  },
+  {
+    ID: 15,
+    projectName: "Speech Recognition",
+    linkURL: "speechRecognition",
+  },
+  {
+    ID: 16,
+    projectName: "Drag And Drop List",
+    linkURL: "dragAndDropList",
+  },
+];
+export default projectDatabase;
--- js/setUpProjects.js
+++ js/setUpProjects.js
@@ -1,1 +1,20 @@
-export default function setUpProjects() {}
+import projectDatabase from "./projectDatabase.js";
+
+export default function setUpProjects() {
+  const buttonWeb = document.getElementById("web");
+  const buttonJavascript = document.getElementById("javascript");
+  const buttonReact = document.getElementById("react");
+  const buttonNodejs = document.getElementById("nodejs");
+  const buttonDjango = document.getElementById("django");
+  const buttonAll = document.getElementById("all");
+
+  buttonWeb.addEventListener("click", (event) => {
+    showProjects();
+  });
+
+  function showProjects(projects) {
+    const projectDisplay = document.getElementById("projectDisplay");
+    let content = "";
+    for (let i = 0; i < projects.length; i++) {}
+  }
+}
