3ba2e9b123f83e1f0d3fb5181386fe361906ca9d
Dom Le <lequydung101@yahoo.com>
2021-01-13 00:28:16 +1100

    change image url

--- js/createMERNProjects.js
+++ js/createMERNProjects.js
@@ -15,16 +15,16 @@ const createMERNProjects = () => {
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="d-block w-100" src="images/MERN/contactKeeper1.png" alt="First slide">
+        <img class="d-block w-100" src="images/MERN/contactkeeper1.png" alt="First slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="images/MERN/contactKeeper2.png" alt="Second slide">
+        <img class="d-block w-100" src="images/MERN/contactkeeper2.png" alt="Second slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="images/MERN/contactKeeper3.png" alt="Third slide">
+        <img class="d-block w-100" src="images/MERN/contactkeeper3.png" alt="Third slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="images/MERN/contactKeeper4.png" alt="4th slide">
+        <img class="d-block w-100" src="images/MERN/contactkeeper4.png" alt="4th slide">
       </div>
     </div>
     <a class="carousel-control-prev " href="#carouselExampleIndicators" role="button" data-slide="prev" style="filter: invert(100%)">
